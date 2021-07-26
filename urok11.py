af7199cfb16f8608261b1b8139985d526ece2002
5avlyha <73108921+5avlyha@users.noreply.github.com>
2021-07-26 10:48:10 +0300

    Create urok11.py

---
+++ urok11.py
@@ -0,0 +1,10 @@
+import random
+chisla=[random.randint(0,99) for i in range(99)]
+print(chisla)
+#Смотрим какие числа есть, и пишем на какие хотим изменить
+a = int(input("Что будем изменять?: "))
+b = int(input("На что меняем?: "))
+for c in range(len(chisla)):
+    if chisla[c] == a:
+        chisla[c] = b
+print(chisla)
